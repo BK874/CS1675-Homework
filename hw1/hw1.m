2978f269ba44d205b4261e65e4f71b045ba41d83
Brian Knotten <bjk69@pitt.edu>
2018-09-04 23:18:27 -0400

    hw1 7-9, 12

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -58,7 +58,7 @@ for row = 1 : rowA
     for col = 1 : colB
         sum = 0;
         for i = 1 : colA
-            sum = sum + A(row, i) * B(i, col)
+            sum = sum + A(row, i) * B(i, col);
         end
         product(row, col) = sum;
     end
@@ -68,20 +68,54 @@ productCheck = A * B
 
 % 7. Show three ways to compute an inner product between two vectors.
 
+% Method 1: A^T * B = inner product of A and B
+A = [1;2;3;4]
+B = [5;6;7;8]
+transpose = A' * B
 
+% Method 2: Use Matlab's dot function
+dotFunc = dot(A, B)
+
+% Method 3: Use loops and calculate it by hand:
+loop = 0;
+for k = 1:A(end)
+    loop = loop + (A(k) * B(k));
+end
+loop
 
 % 8. Compute and print the L1-norm and L2-norm for each of the
 % following two vectors: x1 = [0.5 0 1.5] and x2 = [1 1 0].
 % Verify your answer against the Matlab function norm.
 
+x1 = [0.5, 0, 1.5];
+x2 = [1, 1, 0];
+x1L1 = 0;
+x2L1 = 0;
+x1L2 = 0;
+x2L2 = 0;
 
+for i = 1:3
+    x1L1 = x1L1 + abs(x1(i));
+    x2L1 = x2L1 + abs(x2(i));
+    x1L2 = x1L2 + x1(i)^2;
+    x2L2 = x2L2 + x2(i)^2;
+end
+x1L2 = sqrt(x1L2);
+x2L2 = sqrt(x2L2);
+
+assert(norm(x1,1) == x1L1)
+assert(norm(x2,1) == x2L1)
+assert(norm(x1,2) == x1L2)
+assert(norm(x2,2) == x2L2)
 
 % 9. Use Matlab to solve the system of linear equations:
 % 2x + y + 3z = 1;
 % 2x + 6y + 8z = 3;
 % 6x + 8y + 18z = 5.
 
-
+A = [2 1 3; 2 6 8; 6 8 18];
+B = [1; 3; 5];
+sysLinEq= A\B
 
 % 12. We have the following training set: a red circle, a blue triangle,
 % a blue circle, a green triangle, and a red square. Represent each of
@@ -90,5 +124,13 @@ productCheck = A * B
 % in your representation, i.e. what feature it corresponds to, and what
 % the possible values of that feature are.
 
-
+% The first dimension corresponds to the color of the sample: 0 = red,
+% 1 = blue, 2 = green.
+% The second dimension corresponds to the shape of the sample: 0 = circle,
+% 1 = triangle, 2 = square
+redCircle = [0 0]
+blueTriangle = [1 1]
+blueCircle = [1 0]
+greenTriangle = [2 1]
+redSquare = [0 2]
 
