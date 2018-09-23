ba5df1cf8a2ea24da76922beed3b5ea8a7c1086e
Brian Knotten <bjk69@pitt.edu>
2018-09-23 17:22:45 -0400

    Started Part 2

--- hw3/lr_solve_gd.m
+++ hw3/lr_solve_gd.m
@@ -9,18 +9,24 @@
 % eta = learning rate to use in the weight update
 % w = Dx1 vector of weights (one per feature dimension).
 
-[w] = lr_solve_gd(X_train, y_train, iters, eta)
+function [w] = lr_solve_gd(X_train, y_train, iters, eta)
 
 % Implement the gradient descent solution:
 
 
 % Initialize the weights using random values or all zeros
 
-
+sizeX = size(X_train);
+w = zeros(1, sizeX(2));
 
 % Repeat the following iters times:
 % In each iteration, compute the loss function gradient using all training data points
 % You'll need to use lr_predict.m
+
+diffY = 0;
+				% For each iteration
+diffY = y_train - sum(lr_predict(X_train, w));
+w = w + y_train * X_train * eta;
 
 
 
