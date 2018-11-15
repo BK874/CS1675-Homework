ae06b5463768cc9178f43728dff8130ca9132e8b
Brian Knotten <bjk69@pitt.edu>
2018-11-15 02:06:29 -0500

    hw7 nearly done hopefully

---
+++ hw7/backward.m
@@ -0,0 +1,66 @@
+% Brian Knotten
+% CS1675
+% Homework 7
+
+% X = NxD feature matrix where N is the number of samples and D is the number of feature dimensions
+% y = Nx1 vector containing the ground-truth labels for the N samples
+% M = scalar indicating the number of hidden neurons to use
+% iters = scalar indicating the number of iterations to run (one sample used in each)
+% eta = scalar indicating the learning rate to use
+% W1 = MxD weight matrix between the first and second layer of the network, where M is the number of hidden neurons
+% W2 = 1xM weight matrix between the second and third layer of the network, where there is a single neuron at the output layer
+% error_over_time = iters x 1 vector that contains the error on the sample used in each iteration
+
+function [W1, W2, error_over_time] = backward(X, y, M, iters, eta)
+
+  % Get necessary dimensions
+  XSize = size(X);
+  N = XSize(1);
+  D = XSize(2);
+  
+  % Initialize weights
+  W1 = rand(M, D);
+  W2 = rand(1, M);
+
+  % Initialize the error for each iteration
+  error_over_time = zeros(iters, 1);
+  
+  
+  for iter = 1:iters
+    % Pick a random sample
+    sample = randi(N);
+
+    % Perform forward propogation 
+    [predOutput, Z] = forward(X, W1, W2);
+
+    % Compute error at output
+    outputErr = y(sample) - predOutput(sample);
+    error_over_time(iter) = outputErr;
+
+    % Backpropogate to the hidden layer
+    interW2 = zeros(1, M); % To store weights temporarily
+    interW1 = zeros(M, D);
+
+    % Multiply the activations for this sample for all neurons by the
+    % error for this sample and the learning rate i.e. multiply the
+    % learning rate by the derivative of the Error function
+    lrDeriv = eta * outputErr * Z(sample, :);
+    % Subtract this from the current weights to calculate the new weights
+    interW2 = W2 - lrDeriv;
+
+    % Compute the error at the hidden layer
+    % Square the hidden layer's activations, subtract it from 1, and multiply
+    % the result by the weights from the hidden layer to the output layer and 
+    % the output's error
+    hiddenErr = (1 - (Z(sample, :).^2)) .* W2 * outputErr;
+
+    % Backpropogate to the input layer
+    % Subtract the learning rate mulitiplied by the transpose of the 
+    % hidden layer's error times the current sample
+    interW1 = W1 - eta * hiddenErr' * X(sample, :);
+
+    % Update weights
+    W1 = interW1;
+    W2 = interW2;
+  end
+  
---
+++ hw7/neural_net.m
@@ -0,0 +1,54 @@
+% Brian Knotten
+% CS1675
+% Homework 7
+
+% Use the Wine Quality dataset from HW3 to test the neural implementation
+
+% Load & split the wine dataset as in HW3:
+
+wineData = dlmread('winequality-red.csv', ';', 1, 0);
+wineSize = size(wineData);
+
+%Randomly permute the data before dividing
+randWine = wineData(randperm(wineSize(1)), :);
+
+%Create training set and test set
+trainSet = randWine(1:floor(end/2), 1:end-1);
+testSet = randWine(floor(end/2)+1:end, 1:end-1);
+
+%Define labels
+trainLabels = randWine(1:floor(end/2), end);
+testLabels = randWine(floor(end/2)+1:end, end);
+
+%Compute mean and standard deviation of the training set's feature dimensions
+trainMean = mean(trainSet);
+trainStd = std(trainSet);
+
+%Standardize data 
+stdTrainSet = (trainSet - trainMean)./trainStd;
+stdTestSet = (testSet - trainMean)./trainStd;
+
+%Add bias
+%bStdTrainSet = [ones(size(stdTrainSet, 1), 1) stdTrainSet];
+%bStdTestSet = [ones(size(stdTestSet, 1), 1) stdTestSet];
+
+% Set the hidden units, iterations, and learning rate.
+M = 30;
+iters = 1000;
+eta = 0.01;
+
+% Construct and train the network.
+[W1, W2, error_over_time] = backward(stdTrainSet, trainLabels, M, iters, eta);
+
+% Make predictions and compute root mean squared error between the predicted
+% and the ground-truth labels.
+[y_pred, Z] = forward(stdTestSet, W1, W2);
+error = sqrt(mean((y_pred - testLabels').^2));
+
+% Plot
+figure
+plot(error_over_time) 
+
+
+
+
