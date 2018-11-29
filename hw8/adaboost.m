4eb9a904b986d480822bcd9ca53fd85a9263f2f1
Brian Knotten <bjk69@pitt.edu>
2018-11-28 22:32:45 -0500

    % Project 8 part 1 and 2 done(?)

--- hw8/adaboost.m
+++ hw8/adaboost.m
@@ -12,11 +12,45 @@
 %  all iters classifiers
 
 function [y_pred_final] = adaboost(X_train, y_train, X_test, iters)
+  
+  % Get necessary dimensions
+  trainSize = size(X_train);
+  testSizes = size(X_test);
+  N = trainSize(1);
+  D = trainSize(2);
+  M = testSize(1);
 
-
-
-
-
+  % Initialize weights, alpha vector, and X_test prediction matrix
+  weights = zeros(N, 1) + 1/N; 
+  alpha = zeros(iters, 1);
+  testPred = zeros(M, iters);
+  for it = 1:iters
+    % Determine best decision stump and retrieve its predictions and training
+    % accuracies
+    [correct_train, y_pred] = decision_stump_set(X_train, y_train, weights,
+						 X_test);
+    testPred(:, it) = y_pred;
+    % Switch the accuracies for the indicator function and calculate epsilon
+    % and alpha
+    incorrect_train = abs(correct_train - 1);
+    epsilon = sum(weights .* incorrect_train)/sum(weights);
+    alpha(it) = log((1-epsilon)/epsilon);
+    % If alpha is less than 0, discard the current iteration
+    if alpha(it) < 0
+      alpha(it) = 0;
+      continue
+    end
+    
+    % Update the weights and normalize
+    weights = weights .* exp(alpha(it) .* incorrect_train);
+    weights = weights / sum(weights);
+  end
+  
+% Make final predictions
+  prod = alpha .* testPred;
+  finalPred = sign(sum(prod));
+  finalPred(~final) = 1;
+		  
 
 
 
--- hw8/decision_stump_set.m
+++ hw8/decision_stump_set.m
@@ -2,7 +2,7 @@
 % CS1675
 % Homework 8
 
-% X_train = NxD matri of training samples, N = num training samples, D = num
+% X_train = NxD matrix of training samples, N = num training samples, D = num
 %  feature dimension
 % y_train = Nx1 vector of ground-truth labels for the training set
 % w_train = Nx1 vector containing the weights of the N training samples
@@ -14,8 +14,89 @@
 
 function [correct_train, y_pred] = decision_stump_set(X_train, y_train, w_train,
 						       X_test)
+% Get necessary dimensions
+  trainSize = size(X_train);
+  testSizes = size(X_test);
+  N = trainSize(1);
+  D = trainSize(2);
+  M = testSize(1);
   
+  % Create a Dx10 matrix to store the thresholds of each of the 10 classifiers
+  % for each dimension
+  thresholds = zeros(D, 10);
+  for i = 1:D
+    % Get the min and max of each dimension
+    dimMax = max(X_train(:, i));
+    dimMin = min(X_train(:, i));
+    % Calculate the threshold step sizes
+    threshStep = (dimMax-dimMin)/11;
+    % Fill the threshold maxtrix
+    for j = 1:10
+      thresholds(i, j) = threshStep * j;
+    end
+  end
 
+  % Create a Dx10x2 matrix to store the weighted accuracies of each classifier
+  % The first layer of the third dimension is the accuracy when a feature value
+  % >= the threshold is classified as +1, and the second layer is when the same
+  % feature value is classified as -1
+  wAccur = zeros(D, 10, 2);
+  % For each classifier (10 for each dimension)
+  for i = 1:D
+    for j = 1:10
+      % For each sample
+      for n = 1:N
+	% If the feature value is above the threshold, then the classifier labels
+	% the sample as 1
+	if X_train(n, i) >= threshold(i, j)
+	  tempLabel = 1;
+	else % It is labelled as -1
+	  tempLabel = -1;
+	end
+	% If the label is correct, then the classifiers weight is incremented
+	if tempLabel == y_train(n) 
+	  wAccur(i, j, 1) = wAccur(i, j, 1) + w_train(n);
+	end
+      end
+    end
+  end
+  % The weighted accuracy of the inverse (-1 if above the threshold, 1 if below)
+  % is just 1 - the accuracy of 1 if above, -1 if below.
+  wAccur(:, :, 2) = 1 - wAccur(:, :, 1);
+  
+  % Determine the location of the best classifier by taking the max of the
+  % weighted accuracy matrix
+  [maxVal, index] = max(wAccur(:));
+  [row, column, layer] = ind2sub(size(wAccur), index);
 
+  % Create the column vectors to store the predicted labels and accuracy labels
+  % of the best classifier 
+  y_pred = zeros(M, 1);
+  correct_train = zeros(N, 1);
+  
+  % Variable to determine what the labels above/below the threshold should be
+  invert = 1;
+  if layer == 2
+    invert = -1;
+  end
 
-
+  % For each test sample, use the best classifier to predict the proper label
+  for m = 1:M
+    if X_test(m, row) >= threshold(row, column)
+      y_pred(m) = 1 * invert;
+    else
+      y_pred(m) = -1 * invert;
+    end
+  end
+  % For each train sample, use the best classifier to predict the proper label
+  % and check the correctness
+  for n = 1:N
+    if X_train(n, row) >= threshold(row, column)
+      tempTrainLabel = 1 * invert;
+      correct_train(n) = tempTrainLabel == y_train(n);
+    else
+      tempTrainLabel = -1 * invert;
+      correct_train(n) = tempTrainLabel == y_train(n);
+    end
+  end
+    
